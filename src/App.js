05fbe4cf89a9c039214982146ce9071972acf61e
RavanIsmayilov <rvanismayilov70@gmail.com>
2024-02-16 23:28:29 +0400

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
+import {useEffect, useState} from "react"
 
 function App() {
+
+  const [list , setList] = useState([])
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  
+
+  const fetchUsers = async() =>{
+    try {
+      const response = await axios("https://jsonplaceholder.typicode.com/users")
+        setList(response.data)
+        setFilteredUsers(response.data); 
+        
+    } catch (error) {
+      console.error(error);
+      
+    }
+
+  }
+
+  useEffect(()=>{
+    fetchUsers();
+
+    return() => console.log("unsubscribe")  
+  },[])
+
+
+
+  const handleChange = (e) =>{
+    e.preventDefault()
+
+    const searchTerm = e.target.value;
+    setSearchTerm(searchTerm);
+    filterUsers(searchTerm);
+    
+  }
+
+  const filterUsers = (search) => {
+    const filtered = list.filter(item =>
+      item.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredUsers(filtered);
+  }
+
+
+  
+  const getFunction = () =>{
+
+    return filteredUsers.map((item) => (
+      <div key={item.id}>
+        <h1>Name = {item.name}</h1>
+      </div>
+    ))
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input type="text" name='text' value={searchTerm} placeholder='Search' onChange={handleChange} />
+        {getFunction()}
     </div>
   );
 }
